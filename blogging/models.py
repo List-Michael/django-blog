aa5153ce30ab2d8732236ebcdadef4dfc64934dd
Michael List <michael.list.work@gmail.com>
2020-10-31 11:15:31 -0700

    created blog models

--- blogging/models.py
+++ blogging/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField.auto_now_add()
+    modified_date = models.DateTimeField.auto_now()
+    published_date = models.DateTimeField(blank=True, null=True)
+    score = models.IntegerField(default=0)
