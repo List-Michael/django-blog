55b16b26849f6038807e074bab25c219dc6cc51d
Michael List <michael.list.work@gmail.com>
2020-11-01 10:14:16 -0800

    added ModelAdmin views

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class MembershipInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
