8818093c2f95140650c6d66d33381a312a7fba0b
Michael List <michael.list.work@gmail.com>
2020-11-11 17:29:50 -0800

    added stubs for class based views

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.view.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+class PostListView(ListView):
+    pass
+
+class PostDetailView(DetailView):
+    pass
 
 #def list_view(request):
 #    published = Post.objects.exclude(published_date__exact=None)
